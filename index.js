386fd6f0c0706018c024a65917c9ced3bcc1cb2a
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-09 11:05:36 +0600

    added single service link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -11,23 +11,43 @@ app.use(express.json());
 
 // todo MONGODB
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4n58g.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4n58g.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        const serviceCollection = client.db('walidCollection').collection('services');
 
+        // todo get all the services
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //todo get specific service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 // todo root
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("This is my Assignment 11 server!");
 });
 
 // todo port running on,,,
-app.listen( port, () =>{
+app.listen(port, () => {
     console.log('server is running on port: ', port);
 })
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 [
     {
         "service_id": "04",
-        "title": "Engine Oil Change",
+        "title": "Children’s routine checkups",
         "img": "https://i.ibb.co/T2cpBd5/888.jpg",
         "price": "20.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Pediatricians are medical doctors who focus on infants, children, and adolescents. However, pediatricians aren’t the only ones who can provide care for children. Family medicine doctors can care for your entire family -- from your littles member to your oldest. ",
         "facility": [
             {
                 "name": "Instant Car Services",
@@ -27,10 +27,10 @@
     {
         
         "service_id": "05",
-        "title": "Battery Charge",
+        "title": "Women’s health care",
         "img": "https://i.ibb.co/ydCbDN3/5555.jpg",
         "price": "20.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Women’s health care services can help detect and prevent women-specific health conditions. Examples of women’s health care services include breast exams and Pap smears. Because women are more at risk for developing certain conditions, like osteoporosis, we take care in monitoring any red flags. Bone density scans, for instance, can monitor your bone health. ",
         "facility": [
             {
                 "name": "Instant Car Services",
@@ -53,10 +53,10 @@
     {
         
         "service_id": "01",
-        "title": "Full car Repair",
+        "title": "Men’s health care",
         "img": "https://i.ibb.co/R6Z2nFM/55.jpg",
         "price": "200.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Just like women are more at risk for developing certain conditions, men, too, need specialized health care. Prostate cancer and benign prostate enlargement are two examples of conditions that impact men. Additionally, heart disease is the leading cause of death for men in America, and routine care can help track any signs of developing disease.",
         "facility": [
             {
                 "name": "Instant Car Services",
@@ -79,10 +79,10 @@
     {
         
         "service_id": "02",
-        "title": "Engine Repair",
+        "title": "Mental health care",
         "img": "https://i.ibb.co/5MvmD2g/88.jpg",
         "price": "150.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Family medicine doesn’t just focus on your physical well-being. We focus on your whole person, which includes mental and physical health. Depression and anxiety are extremely common. Anxiety disorders alone affect 40 million Americans. Because depression and anxiety can be managed with lifestyle changes and medication, family medicine can ensure that all aspects of your health are covered.",
         "facility": [
             {
                 "name": "Instant Car Services",
@@ -105,10 +105,10 @@
     {
         
         "service_id": "03",
-        "title": "Automatic Services",
+        "title": "Sports physicals",
         "img": "https://i.ibb.co/wh7t3N3/555.jpg",
         "price": "30.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Sports physicals are designed to assess your health to make sure you or your child are healthy enough to participate in your favorite sport or recreational activity. These, too, are available here at Preva Surgicare - Surgery Center Of The Woodlands.",
         "facility": [
             {
                 "name": "Instant Car Services",
@@ -131,10 +131,10 @@
     {
         
         "service_id": "06",
-        "title": "Electrical System",
+        "title": "Chronic disease management",
         "img": "https://i.ibb.co/KzCG8qr/8888.jpg",
         "price": "20.00",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+        "description": "Chronic diseases can’t be cured, but ongoing management can help you avoid the unwanted complications of unmanaged chronic health conditions. Examples of chronic health conditions include asthma, diabetes, arthritis, and thyroid disease.",
         "facility": [
             {
                 "name": "Instant Car Services",
